479d98c58dbd41b173ef77372a5e529266f299b8
jstrachan <>
2001-01-08 18:45:43 +0000

    Added some more helper methods

--- src/java/org/dom4j/DocumentFactory.java
+++ src/java/org/dom4j/DocumentFactory.java
@@ -21,8 +21,13 @@ public class DocumentFactory {
     protected XPathEngine XPathEngine;
     
     // Some static helper methods
+    
     public static Document create() {
         return singleton.createDocument();
+    }
+    
+    public static Document create(Element rootElement) {
+        return singleton.createDocument(rootElement);
     }
     
     /** <p>Access to singleton implementation of DocumentFactory which 
@@ -66,6 +71,13 @@ public class DocumentFactory {
     public Document createDocument() {
         Document answer = new DefaultDocument();
         answer.setXPathEngine( getXPathEngine() );
+        return answer;
+    }
+    
+    
+    public Document createDocument(Element rootElement) {
+        Document answer = createDocument();
+        answer.setRootElement(rootElement);
         return answer;
     }
     
