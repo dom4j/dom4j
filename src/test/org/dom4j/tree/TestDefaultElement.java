1d32a24968e300ed60f0f5182679fa8513e94875
maartenc <>
2004-03-12 16:44:21 +0000

    Added test case for bug 894878 reported by Stephane Toussaint http://sourceforge.net/tracker/index.php?func=detail&aid=894878&group_id=16035&atid=116035

---
+++ src/test/org/dom4j/tree/TestDefaultElement.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2001-2004 (C) MetaStuff, Ltd. All Rights Reserved.
+ * 
+ * This software is open source. 
+ * See the bottom of this file for the licence.
+ * 
+ * $Id$
+ */
+
+package org.dom4j.tree;
+
+import junit.framework.*;
+import junit.textui.TestRunner;
+import org.dom4j.*;
+
+/**
+ *
+ * @author Maarten Coene
+ */
+public class TestDefaultElement extends AbstractTestCase {
+
+    public static void main( String[] args ) {
+        TestRunner.run( suite() );
+    }
+    
+    public static Test suite() {
+        return new TestSuite( TestDefaultElement.class );
+    }
+    
+    public TestDefaultElement(String name) {
+        super(name);
+    }
+
+    // Test case(s)
+    //-------------------------------------------------------------------------
+    public void testBug894878() {
+        Element foo = DocumentFactory.getInstance().createElement("foo");
+        foo.addText("bla").addAttribute("foo", "bar");
+        assertEquals("<foo foo=\"bar\">bla</foo>", foo.asXML());
+        
+        foo = DocumentFactory.getInstance().createElement("foo");
+        foo.addAttribute("foo", "bar").addText("bla");
+        assertEquals("<foo foo=\"bar\">bla</foo>", foo.asXML());
+    }
+    
+}
