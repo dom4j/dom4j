124c82d26439d0da443ff48daf3b328bead326c6
maartenc <>
2004-06-04 08:27:21 +0000

    improved performance in multi-threaded environments by using a specialized concurrent Map implementation instead of making a whole method synchronized

--- src/java/org/dom4j/tree/NamespaceCache.java
+++ src/java/org/dom4j/tree/NamespaceCache.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2001 (C) MetaStuff, Ltd. All Rights Reserved.
+ * Copyright 2001-2004 (C) MetaStuff, Ltd. All Rights Reserved.
  *
  * This software is open source.
  * See the bottom of this file for the licence.
@@ -9,39 +9,70 @@
 
 package org.dom4j.tree;
 
+import java.lang.reflect.Constructor;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.dom4j.Namespace;
 
-/** <p><code>NamespaceCache</code> caches instances of <code>DefaultNamespace</code>
-  * for reuse both across documents and within documents.</p>
-  *
-  * @author <a href="mailto:james.strachan@metastuff.com">James Strachan</a>
-  * @version $Revision$
-  */
+/** 
+ * <p><code>NamespaceCache</code> caches instances of <code>DefaultNamespace</code>
+ * for reuse both across documents and within documents.</p>
+ *
+ * @author <a href="mailto:james.strachan@metastuff.com">James Strachan</a>
+ * @author Maarten Coene
+ * @author Brett Finnell
+ * @version $Revision$
+ */
 public class NamespaceCache {
 
     /** Cache of {@link Map} instances indexed by URI which contain
       * caches of {@link Namespace} for each prefix
       */
-    protected static Map cache;
+    protected static Map cache; 
 
     /** Cache of {@link Namespace} instances indexed by URI
       * for default namespaces with no prefixes
       */
     protected static Map noPrefixCache;
 
+    static {
+        /* Try the java.util.concurrent.ConcurrentHashMap first. */
+        try {
+            Class clazz = Class.forName("java.util.concurrent.ConcurrentHashMap");
+            Constructor construct = clazz.getConstructor(new Class[] {Integer.TYPE, Float.TYPE, Integer.TYPE});
+            cache = (Map) construct.newInstance(new Object[] {new Integer(11), new Float(0.75), new Integer(1)});
+            noPrefixCache = (Map) construct.newInstance(new Object[] {new Integer(11), new Float(0.75), new Integer(1)});
+        } catch (Exception exc1) {
+            /* Try to use the util.concurrent library (if in classpath) */
+            try {
+                Class clazz = Class.forName("EDU.oswego.cs.dl.util.concurrent.ConcurrentReaderHashMap");
+                cache = (Map) clazz.newInstance();
+                noPrefixCache = (Map) clazz.newInstance();
+            } catch (Exception exc2) {
+                /* If previous implementations fail, use internal one */
+                cache = new ConcurrentReaderHashMap();
+                noPrefixCache = new ConcurrentReaderHashMap();
+            }
+        }
+       
+    }
+
 
     /** @return the name model for the given name and namepsace
       */
-    public synchronized Namespace get(String prefix, String uri) {
+    public Namespace get(String prefix, String uri) {
         Map cache = getURICache(uri);
         Namespace answer = (Namespace) cache.get(prefix);
         if (answer == null) {
-            answer = createNamespace(prefix, uri);
-            cache.put(prefix, answer);
+            synchronized (cache) {
+                answer = (Namespace) cache.get(prefix);
+                if (answer == null) {
+                    answer = createNamespace(prefix, uri);
+                    cache.put(prefix, answer);
+                }
+            }
         }
         return answer;
     }
@@ -49,14 +80,16 @@ public class NamespaceCache {
 
     /** @return the name model for the given name and namepsace
       */
-    public synchronized Namespace get(String uri) {
-        if ( noPrefixCache == null ) {
-            noPrefixCache = createURIMap();
-        }
+    public Namespace get(String uri) {
         Namespace answer = (Namespace) noPrefixCache.get(uri);
         if (answer == null) {
-            answer = createNamespace("", uri);
-            noPrefixCache.put(uri, answer);
+            synchronized (noPrefixCache) {
+                answer = (Namespace) noPrefixCache.get(uri);
+                if (answer == null) {
+                    answer = createNamespace("", uri);
+                    noPrefixCache.put(uri, answer);
+                }
+            }
         }
         return answer;
     }
@@ -65,14 +98,16 @@ public class NamespaceCache {
     /** @return the cache for the given namespace URI. If one does not
       * currently exist it is created.
       */
-    protected synchronized Map getURICache(String uri) {
-        if (cache == null) {
-            cache = createURIMap();
-        }
+    protected Map getURICache(String uri) {
         Map answer = (Map) cache.get(uri);
         if (answer == null) {
-            answer = createPrefixMap();
-            cache.put(uri, answer);
+            synchronized (cache) {
+                answer = (Map) cache.get(uri);
+                if (answer == null) {
+                    answer = new ConcurrentReaderHashMap();
+                    cache.put(uri, answer);
+                }
+            }
         }
         return answer;
     }
@@ -82,19 +117,6 @@ public class NamespaceCache {
       */
     protected Namespace createNamespace(String prefix, String uri) {
         return new Namespace(prefix, uri);
-    }
-    /** A factory method to create prefix caches
-      * @return a newly created {@link Map} instance.
-      */
-    protected Map createPrefixMap() {
-        return Collections.synchronizedMap(new HashMap());
-    }
-
-    /** A factory method to create URI caches
-      * @return a newly created {@link Map} instance.
-      */
-    protected Map createURIMap() {
-        return Collections.synchronizedMap(new HashMap());
     }
 }
 
@@ -126,7 +148,7 @@ public class NamespaceCache {
  *    trademark of MetaStuff, Ltd.
  *
  * 5. Due credit should be given to the DOM4J Project
- *    (http://dom4j.org/).
+ *    http://dom4j.org/
  *
  * THIS SOFTWARE IS PROVIDED BY METASTUFF, LTD. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
@@ -141,7 +163,7 @@ public class NamespaceCache {
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Copyright 2001 (C) MetaStuff, Ltd. All Rights Reserved.
+ * Copyright 2001-2004 (C) MetaStuff, Ltd. All Rights Reserved.
  *
  * $Id$
  */
