9ea0fe1506256f9f26266553ee69be8d6148e9fe
maartenc <>
2004-06-17 12:29:18 +0000

    Added method to Element to retrieve all Namespaces for a given URI http://sourceforge.net/tracker/index.php?func=detail&aid=965893&group_id=16035&atid=116035

--- src/test/org/dom4j/tree/TestDefaultElement.java
+++ src/test/org/dom4j/tree/TestDefaultElement.java
@@ -9,6 +9,7 @@
 
 package org.dom4j.tree;
 
+import java.util.List;
 import junit.framework.*;
 import junit.textui.TestRunner;
 import org.dom4j.*;
@@ -43,4 +44,23 @@ public class TestDefaultElement extends AbstractTestCase {
         assertEquals("<foo foo=\"bar\">bla</foo>", foo.asXML());
     }
     
+    public void testGetNamespacesForURI() throws Exception {
+        String xml = 
+                "<schema targetNamespace='http://SharedTest.org/xsd' " +
+                "        xmlns='http://www.w3.org/2001/XMLSchema' " +
+                "        xmlns:xsd='http://www.w3.org/2001/XMLSchema'>" +
+                "    <complexType name='AllStruct'>" +
+                "        <all>" +
+                "            <element name='arString' type='xsd:string'/>" +
+                "            <element name='varInt' type='xsd:int'/>" +
+                "        </all>" +
+                "    </complexType>" +
+                "</schema>";
+        Document doc = DocumentHelper.parseText(xml);
+        Element schema = doc.getRootElement();
+        List namespaces = schema.getNamespacesForURI("http://www.w3.org/2001/XMLSchema");
+        
+        assertNotNull(namespaces);
+        assertEquals(2, namespaces.size());
+    }
 }
