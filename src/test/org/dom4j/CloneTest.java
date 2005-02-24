10e6ccd42c0f81a1f3d91ec537858a8959fd1d20
maartenc <>
2005-02-24 19:10:08 +0000

    Added some extra clone tests.

--- src/test/org/dom4j/CloneTest.java
+++ src/test/org/dom4j/CloneTest.java
@@ -11,6 +11,9 @@ import junit.textui.TestRunner;
 
 import java.util.Comparator;
 
+import org.dom4j.dom.DOMDocument;
+import org.dom4j.dom.DOMDocumentFactory;
+import org.dom4j.dom.DOMNamespace;
 import org.dom4j.io.OutputFormat;
 import org.dom4j.io.XMLWriter;
 import org.dom4j.util.NodeComparator;
@@ -32,6 +35,23 @@ public class CloneTest extends AbstractTestCase {
 
     // Test case(s)
     // -------------------------------------------------------------------------
+    public void testBug1148333() {
+        DOMDocumentFactory factory = (DOMDocumentFactory) DOMDocumentFactory.getInstance();
+        DOMDocument doc = (DOMDocument) factory.createDocument();
+        Element el = doc.addElement("root");
+        el.addNamespace("pref2", "uri2");
+        
+        doc.cloneNode(true);
+    }
+    
+    public void testElementWithNamespaceClone() {
+        Element element = DocumentFactory.getInstance().createElement("element");
+        element.addNamespace("prefix", "uri");
+        Element clone = (Element) element.clone();
+        
+        assertNodesEqual(element, clone);
+    }
+    
     public void testDocumentClone() throws Exception {
         document.setName("doc1");
 
