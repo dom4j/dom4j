dc106abbb7581b352b3f0445a72ea87f8ea8656d
Filip Jirsák <filip@jirsak.org>
2016-04-07 15:07:54 +0200

    move package javadocs to package-info.java

---
+++ src/main/java/org/dom4j/datatype/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * An implementation of the <i>dom4j</i> API which supports the <a href="http://www.w3.org/TR/xmlschema-2/">XML Schema Data Types</a> specification.
+ */
+package org.dom4j.datatype;
---
+++ src/main/java/org/dom4j/swing/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * A collection of adapters to allow easy integration with dom4j XML documents and Swing such as TreeModels and TableModels.
+ */
+package org.dom4j.swing;
