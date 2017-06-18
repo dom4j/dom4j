a37228d4ca40c842f8993e729cb35db6dac83fbd
Filip Jirsák <filip@jirsak.org>
2017-06-18 02:10:47 +0200

    #20 Use "new" (since Java 1.4) Exception constructor.

--- src/main/java/org/dom4j/DocumentException.java
+++ src/main/java/org/dom4j/DocumentException.java
@@ -7,76 +7,32 @@
 
 package org.dom4j;
 
-import java.io.PrintStream;
-import java.io.PrintWriter;
-
 /**
- * <code>DocumentException</code> is a nested Exception which may be thrown
- * during the processing of a DOM4J document.
+ * <code>DocumentException</code> is a nested Exception which may be thrown during the processing of a DOM4J document.
  *
  * @author <a href="mailto:james.strachan@metastuff.com">James Strachan </a>
- * @version $Revision: 1.7 $
+ * @author Filip Jirsák
  */
 public class DocumentException extends Exception {
-    /** A wrapped <code>Throwable</code> */
-    private Throwable nestedException;
 
     public DocumentException() {
-        super("Error occurred in DOM4J application.");
     }
 
     public DocumentException(String message) {
         super(message);
     }
 
-    public DocumentException(Throwable nestedException) {
-        super(nestedException.getMessage());
-        this.nestedException = nestedException;
+    public DocumentException(String message, Throwable cause) {
+        super(message, cause);
     }
 
-    public DocumentException(String message, Throwable nestedException) {
-        super(message);
-        this.nestedException = nestedException;
+    public DocumentException(Throwable cause) {
+        super(cause);
     }
 
+    @Deprecated
     public Throwable getNestedException() {
-        return nestedException;
-    }
-
-    public String getMessage() {
-        if (nestedException != null) {
-            return super.getMessage() + " Nested exception: "
-                    + nestedException.getMessage();
-        } else {
-            return super.getMessage();
-        }
-    }
-
-    public void printStackTrace() {
-        super.printStackTrace();
-
-        if (nestedException != null) {
-            System.err.print("Nested exception: ");
-            nestedException.printStackTrace();
-        }
-    }
-
-    public void printStackTrace(PrintStream out) {
-        super.printStackTrace(out);
-
-        if (nestedException != null) {
-            out.println("Nested exception: ");
-            nestedException.printStackTrace(out);
-        }
-    }
-
-    public void printStackTrace(PrintWriter writer) {
-        super.printStackTrace(writer);
-
-        if (nestedException != null) {
-            writer.println("Nested exception: ");
-            nestedException.printStackTrace(writer);
-        }
+        return null;
     }
 }
 
